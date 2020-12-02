0423e0b9c8c7e7249a9c6afdc4975324aadc8ba3
daviddonkor <donkorsiawdavid@gmail.com>
2020-12-02 19:55:30 +0000

    After two lessons in the tutorial

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express=require("express");
-
+const connectDB= require("./config/db");
 const app=express();
 
+//Connect to Database
+connectDB();
+app.use(express.json({extended:false}));
 app.get('/',(req,res)=>res.send('API Running'));
+
+//Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/story',require('./routes/api/story'));
+app.use('/api/chainstory',require('./routes/api/chainstory'));
+app.use('/api/videostory',require('./routes/api/videostory'));
+app.use('/api/picturestory',require('./routes/api/picturestory'));
+app.use('/api/comment',require('./routes/api/comment'));
+ 
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
